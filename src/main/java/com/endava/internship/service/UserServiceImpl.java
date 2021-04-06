048d0be8a4bbf6c17927c9d939946142a6c91457
CasAlexei <hp81@mail.ru>
2021-04-06 16:14:30 +0300

    groupByPrivileges not implement

--- src/main/java/com/endava/internship/service/UserServiceImpl.java
+++ src/main/java/com/endava/internship/service/UserServiceImpl.java
@@ -14,41 +14,33 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<String> getFirstNamesReverseSorted(List<User> users) {
-        List<String> newList = users.stream()
+        return users.stream()
                 .map(name -> name.getFirstName())
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
-
-        return newList;
     }
 
     @Override
     public List<User> sortByAgeDescAndNameAsc(final List<User> users) {
-        List<User> newList = users.stream()
+        return users.stream()
                 .sorted(Comparator.comparingInt(User::getAge).reversed().thenComparing(User::getFirstName))
                 .collect(Collectors.toList());
-
-        return newList;
     }
 
     @Override
     public List<Privilege> getAllDistinctPrivileges(final List<User> users) {
-        List<Privilege> listPrivilege = users.stream()
+        return users.stream()
                 .map(User::getPrivileges)
                 .flatMap(List::stream)
                 .distinct()
                 .collect(Collectors.toList());
-
-        return listPrivilege;
     }
 
     @Override
     public Optional<User> getUpdateUserWithAgeHigherThan(final List<User> users, final int age) {
-        Optional<User> userHigherAge = users.stream()
+        return users.stream()
                 .filter(user -> user.getAge() > age)
                 .findFirst();
-
-        return userHigherAge;
     }
 
     @Override
@@ -59,7 +51,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public double getAverageAgeForUsers(final List<User> users) {
-
         if(users.size() == 0)
             return -1;
         else
@@ -68,7 +59,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<String> getMostFrequentLastName(final List<User> users) {
-
         return users.stream()
                 .collect(Collectors.groupingBy(User::getLastName, Collectors.counting()))
                 .entrySet()
@@ -81,7 +71,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> filterBy(final List<User> users, final Predicate<User>... predicates) {
-
         return users.stream()
                 .filter(Arrays.stream(predicates).reduce(is -> true, Predicate::and))
                 .collect(Collectors.toList());
@@ -89,13 +78,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String convertTo(final List<User> users, final String delimiter, final Function<User, String> mapFun) {
-        throw new UnsupportedOperationException("Not implemented");
+        return users.stream()
+                .map(mapFun)
+                .collect(Collectors.joining(delimiter));
     }
 
     @Override
     public Map<Privilege, List<User>> groupByPrivileges(List<User> users) {
 //        return users.stream()
 //                .collect(Collectors.groupingBy(User::getPrivileges));
+
         throw new UnsupportedOperationException("Not implemented");
     }
 
